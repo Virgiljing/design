2fd73e487686fa3f9fe109fe620007d6d3582be6
hangming <1195223570@qq.com>
2020-06-03 10:00:04 +0800

    算法两数求和

---
+++ src/leetcode/Test09Palindrome.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class Test09Palindrome {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
